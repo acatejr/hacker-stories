17b02c325ead989addf97bd27591913e3922f35d
Averill Cate, Jr <acate@arizona.edu>
2023-02-22 10:23:50 -0700

    Finished up to SVGs in React, p. 180

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import axios from 'axios'
-import './App.css'
+import styles from './App.module.css'
 
 const welcome = {
   greeting: 'Hey',
@@ -172,8 +172,8 @@ const App = () => {
   })
 
   return (
-    <div className='container'>
-      <h1 className='headline-primary'>My Hacker Stories</h1>
+    <div className={styles.container}>
+      <h1 className={styles.headlinePrimary}>My Hacker Stories</h1>
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
@@ -206,7 +206,7 @@ const InputWithLabel = ({id, label, value, type='text', onInputChange, isFocused
   }, [isFocused])
   return (
   <>
-    <label htmlFor={id} className='label'>{children}</label>
+    <label htmlFor={id} className={styles.label}>{children}</label>
     &nbsp;
     <input
       ref={inputRef}
@@ -215,7 +215,7 @@ const InputWithLabel = ({id, label, value, type='text', onInputChange, isFocused
       value={value}
       autoFocus={isFocused}
       onChange={onInputChange}
-      className='input'
+      className={styles.input}
     />
   </>
   )
@@ -247,7 +247,7 @@ const Item = ({item, onRemoveItem}) => {
   }
 
   return (
-    <li className='item'>
+    <li className={styles.item}>
       <span style={{width: '40%'}}>
         <a href={item.url}>{item.title}</a>
       </span>
@@ -255,7 +255,7 @@ const Item = ({item, onRemoveItem}) => {
       <span style={{width: '10%' }}>{item.num_comments}</span>
       <span style={{width: '10%' }}>{item.points}</span>
       <span style={{width: '10%'}}>
-        <button type="button" onClick={() => {onRemoveItem(item)}} className='button button_small'>
+        <button type="button" onClick={() => {onRemoveItem(item)}} className={`${styles.button} ${styles.buttonSmall}`}>
           Dismiss
         </button>
       </span>
@@ -268,7 +268,7 @@ const SearchForm = ({
   onSearchInput,
   onSearchSubmit,
 }) => (
-  <form onSubmit={onSearchSubmit} className='search-form'>
+  <form onSubmit={onSearchSubmit} className={styles.searchForm}>
       <InputWithLabel 
           id="search" 
           value={searchTerm} 
@@ -278,7 +278,7 @@ const SearchForm = ({
           <strong>Search:</strong>
       </InputWithLabel>
 
-      <button type="submit" disabled={!searchTerm} className='button button_large'>
+      <button type="submit" disabled={!searchTerm} className={`${styles.button} ${styles.buttonLarge}`}>
         Submit
       </button>
   </form>
