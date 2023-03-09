6596af153facd5191520d1bc15fd82f6e2a12f48
Averill Cate, Jr <acate@arizona.edu>
2023-03-09 07:38:30 -0700

    Finished up to Integration Testing: Component, p. 228

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -77,3 +77,35 @@ describe('Item', () => {
         expect(handleRemoveItem).toHaveBeenCalledTimes(1)
     })
 })
+
+describe('SearchForm', () => {
+    const searchFormProps = {
+        searchTerm: 'React',
+        onSearchInput: vi.fn(),
+        onSearchSubmit: vi.fn(),
+    }
+
+    it('renders the input field with its value', () => {
+        render(<SearchForm {...searchFormProps} />)
+        expect(screen.getByDisplayValue('React')).toBeInTheDocument()
+    })
+
+    it('renders the correct label', () => {
+        render(<SearchForm {...searchFormProps} />)
+        expect(screen.getByLabelText(/Search/)).toBeInTheDocument()
+    })
+
+    it('calls onSearchInput on input field change', () => {
+        render(<SearchForm {...searchFormProps} />)
+        fireEvent.change(screen.getByDisplayValue('React'), {
+            target: {value: 'Redux'}
+        })
+        expect(searchFormProps.onSearchInput).toHaveBeenCalledTimes(1)
+    })
+
+    it('calls onSearchInput on button submit click', () => {
+        render(<SearchForm {...searchFormProps} />)
+        fireEvent.submit(screen.getByRole('button'))
+        expect(searchFormProps.onSearchSubmit).toHaveBeenCalledTimes(1)
+    })
+})
